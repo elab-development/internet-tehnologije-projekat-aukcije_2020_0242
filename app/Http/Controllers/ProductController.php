ded831c984139fce4d4551de10c2d8f9453fe4e7
Tamara-ai <tb20200296@student.fon.bg.ac.rs>
2023-12-23 14:28:06 +0100

    Kreirane putanje za rad sa proizvodom

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $page = $request->query('page', 0);
+        $size = $request->query('size', 20);
+        $where = [];
+        $name = $request->query('name', '');
+        $sold = $request->query('sold', null);
+        if ($name != '') {
+            $where[] = ['name', 'like', '%' . $name . '%'];
+        }
+        if ($sold != null) {
+            $where[] = ['sold', '=', boolval($sold)];
+        }
+        $products = Product::where($where)->paginate($size, ['*'], 'page', $page);
+        return response()->json($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $body = $request->all();
+        $product = Product::create($body);
+        return response()->json($product);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $body = $request->all();
+        $product->update($body);
+        return response()->json($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->noContent();
+    }
+}
