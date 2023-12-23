b542446b92311be79cde3a9a9acff53cdef59556
anaabasic <ab20200242@student.fon.bg.ac.rs>
2023-12-23 14:07:40 +0100

    Kreiran model Bid

---
+++ app/Models/Bid.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bid extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['price', 'user_id', 'auction_id'];
+
+    protected $casts = [
+        'id' => 'integer',
+        'price' => 'double',
+        'user_id' => 'integer',
+        'auction_id' => 'integer'
+    ];
+}
